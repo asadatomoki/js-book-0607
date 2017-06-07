d9274edacde2743bb3b1fbf720db15d0adbb3259
LAPTOP-7EKS3K5H\tomoki <dorazen0309@gmail.com>
2017-06-07 12:04:36 +0900

    6/7授業終了　p.oo

--- list4-1.js
+++ list4-1.js
@@ -6,3 +6,10 @@ function sum() {
 
     window.alert(total);
 }
+function sum() {
+  var total = 0;
+  for (var counter = 1; conter <= 10; counter++) {
+      total +=counter;
+  }
+  window.alert(total);
+}
